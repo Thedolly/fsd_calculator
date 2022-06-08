d4f78ccd1f986beb8dbb8c957fe2ba8315dd0fd5
dollytiwari <dollytiwari393@gmail.com>
2022-06-08 13:18:10 +0530

    calculator first commit

---
+++ calculator.js
@@ -0,0 +1,19 @@
+function add(a,b){
+    var resAdd = a+b;
+    console.log("addition = "+resAdd);
+    if(a<b){
+        var resMinus = b-a;
+    }else {
+        var resMinus = a-b;
+    }  
+    console.log("subtration = "+resMinus);
+    var resMul = a*b;
+    console.log("Multiplication = "+resMul);
+    var divRemain = a%b;
+    console.log("After division remainder = "+divRemain);
+    var div = a/b;
+    console.log("division = "+div);
+
+}
+add(2,4);
+add(2,2);
---
+++ jsintro.js
@@ -0,0 +1,14 @@
+function add(a,b){
+    var ans= a+b;
+    return ans;
+    //console.log(ans);
+}
+
+var x = add(3,5);
+console.log(x);
+
+var num = "dolly";
+var age = 26;
+
+console.log(num);
+console.log(age);
